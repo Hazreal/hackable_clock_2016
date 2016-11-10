aadcc6bf8e8ce7fd5a78c0b6cfaa654eed542fb0
Adam <cloakedzargon@gmail.com>
2016-11-09 20:49:40 -0600

    added additional definitions and and function for writing to real time clock

--- clock_defs.h
+++ clock_defs.h
@@ -31,3 +31,48 @@
 #define OFF 0
 
 #endif
+
+#define CMD_RITE_SECS 0x80
+#define CMD_READ_SECS 0x81
+#define CMD_RITE_MINS 0x82
+#define CMD_READ_MINS 0x83
+#define CMD_RITE_HOUR 0x84
+#define CMD_READ_HOUR 0x85
+#define CMD_RITE_DAY  0x86
+#define CMD_READ_DAY  0x87
+#define CMD_RITE_MON  0x88
+#define CMD_READ_MON  0x89
+#define CMD_RITE_DOW  0x8a
+#define CMD_READ_DOW  0x8b
+#define CMD_RITE_YEAR 0x8c
+#define CMD_READ_YEAR 0x8d
+#define CMD_WRITE_TCS 0x90
+
+void writeRTC(unsigned char command, unsigned long data, unsigned short bits);
+
+{
+  int  bit;
+  
+  assert(bits < 32);
+  
+  high(RTC_CE)
+  for(int x=0; x<8; x++)
+  {
+    bit = command & 0x01;
+    output(RTC_DAT, bit)
+    high(RTC_CLK);
+    low(RTC_CLK);
+    command = command >> 1;
+  }
+  
+  for(int x=0; x<bits; x++)
+  {
+    bit = data & 0x01;
+    low(RTC_CLK);
+    output(RTC_DAT, bit)
+    high(RTC_CLK);
+    command = data >> 1;
+  }      
+    
+  low(RTC_CE)
+}  
